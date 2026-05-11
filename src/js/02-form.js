519438a23333908063a0ce8d7e1cfb5771b88c6a
Yeşim <yesimyurdaka1999@outlook.com>
2026-05-11 14:23:38 +0300

    modified

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -6,28 +6,45 @@ const formData = {
     email: "",
     message: "",
 };
-
+//verileri alır
 const getData = JSON.parse(localStorage.getItem(localStorageKey));
-const email = getData.email || "";
-const message = getData.message || "";
 
-Yani:
-//form.elements.email.value = localStorage.getItem(localStorageKey) ?? "";
-//form.elements.message.value = localStorage.getItem(localStorageKey)
-//  ?? "";
+const email = (getData && getData.email) || "";
+const message = (getData && getData.message) || "";
 
-form.addEventListener("input", (event) => {
+
+form.elements.email.value = email;
+form.elements.message.value = message;
+
+form.addEventListener("input", () => {
+    formData.email = form.elements.email.value.trim();
+    formData.message = form.elements.message.value.trim();
+
     localStorage.setItem(localStorageKey, JSON.stringify(formData));
 });
+
+//formData.email = form.elements.email.value.trim();
+//formData.message = form.elements.message.value.trim();
+
+//localStorage.setItem(localStorageKey, JSON.stringify(formData));
+
+//form.addEventListener("input", (event) => {
+//  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+//});
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const email = form.elements.email.value.trim();
     const message = form.elements.message.value.trim();
+    if (email === "" || message === "") {
+        alert("");
+        return;
+    }
     localStorage.removeItem(localStorageKey);//fonksiyonun içinde yazılmalı
     form.reset();//fonksiyonun içine yazılmalı
-
+    console.log({ email, message });
 });
 
-console.log(formData);
+
+
